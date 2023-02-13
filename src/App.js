4e9a443070c64a889223c03d12e9ad0779c15cd2
Jableman19 <jableman@umich.edu>
2023-02-13 11:55:24 -0500

    Pokemon file names updated to reflect popularity -- fixed flow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import "./App.css";
 import axios from "axios";
+import { Button, Typography } from "@mui/material";
 
 function App() {
 
@@ -8,6 +9,9 @@ function App() {
   const redirectUri = "http://localhost:3000/callback"
   const [accessToken, setAccessToken] = useState(null);
   const [topTracks, setTopTracks] = useState([]);
+  const [popularity, setPopularity] = useState(0);
+  const [popKey, setPopKey] = useState(0);
+  const [mon, setMon] = useState("");
 
   const imagesContext = require.context('./151', true, /\.(png|jpeg|jpg|gif|svg)$/);
   const imageKeys = imagesContext.keys();
@@ -33,13 +37,21 @@ function App() {
 
   const popCalc = (topTracks) => {
     //go through each track and add up the popularity and then divide by the number of tracks
+    console.log(topTracks)
     var totalPop = 0;
     var numTracks = 0;
     topTracks.forEach(track => {
       totalPop += track.popularity;
       numTracks++;
     });
-    return (totalPop/numTracks);
+    setPopularity(totalPop/numTracks);
+    const localPopKey = Math.floor(((totalPop/numTracks)/100) * 151)-1;
+    setPopKey(localPopKey);
+    try{
+      setMon(sortedImageKeys[localPopKey].split("-")[2].split(".")[0])
+    }
+    catch(error){
+    }
   }
 
   const handleCallback = () => {
@@ -57,36 +69,52 @@ function App() {
   };
 
   const getTopTracks = async () => {
-    const response = await axios.get('https://api.spotify.com/v1/me/top/tracks?limit=50', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    });
+    
+    try{
+        const response = await axios.get('https://api.spotify.com/v1/me/top/tracks?limit=50', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+      setTopTracks(response.data.items);
+      popCalc(response.data.items);
 
-    setTopTracks(response.data.items);
+    } catch (error) {
+      setAccessToken(null);
+    }
+
   };
 
   if (!accessToken) {
     return (
       <div>
-        <button onClick={handleLogin}>Login with Spotify</button>
+        <Button onClick={handleLogin}>Login with Spotify</Button>
         {window.location.hash.length > 0 && handleCallback()}
       </div>
     );
   }
 
-  return (
+  else if(!mon){
+    return (
+      <div>
+        <Button onClick={getTopTracks} sx={{display: "block", margin: 'auto', marginTop: '10%'}}>Calculate My Spotify Pokemon</Button>
+      </div>
+    );
+  }
+
+  return(
     <div>
-      <button onClick={getTopTracks}>Get Top Tracks</button>
-      <ul>
+      {/* <ul>
         {topTracks.map(track => (
           <li key={track.id}>{track.name + "popularity: " + track.popularity}</li>
         ))} 
-      </ul>
-        {topTracks ? " total popularity: " + popCalc(topTracks) : ""}
-        {topTracks ? <img src={(images[Math.floor((popCalc(topTracks)/100) * 151)-1])} alt="Pokemon representing popularity" /> : ""}
+      </ul> */}
+        {topTracks.length ? 
+        <Typography>{"The average popularity of your top 50 recent tracks is  " + popularity + " out of 100, this makes you a " + mon + "!"}</Typography> : 
+        <></>}
+        {topTracks.length ? <img src={(images[popKey])} alt="Pokemon representing popularity" /> : <></>}
     </div>
-  );
+  )
 }
 
 export default App;
